4f624fc241324d7f0cd16f9569b5bf13e14c06f5
Dario Segura <dsegura@uncharted.software>
2016-04-27 13:55:38 -0400

    [DIST] Updated index file to just export the index in the `list` folder.

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,1 @@
-/// <reference path="./dist/index.d.ts" />
-
-declare module EssexPBIBase {
-    export { default as Utils, Visual, IDiffProcessor} from 'lib/Utils';
-    export { default as VisualBase, ExternalCssResource} from 'lib/VisualBase';
-}
+export * from "./dist"
