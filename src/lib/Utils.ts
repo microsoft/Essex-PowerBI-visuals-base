1b2d5c56f3c1db6bb36a4e5cd0f1735e653ef04e
darthtrevino <darthtrevino@gmail.com>
2016-10-06 15:43:40 -0700

    Move hasDataChanged and listDiff to top-level exported functions

--- src/lib/Utils.ts
+++ src/lib/Utils.ts
@@ -64,7 +64,7 @@ export function Visual(config: { visualName: string; projectId: string }) {
  */
 export class PropertyPersister {
     constructor(
-        private host: powerbi.IVisualHostServices, // tslint:disable-line 
+        private host: powerbi.IVisualHostServices, // tslint:disable-line
         private delay: number = 100 // tslint:disable-line
     ) {}
 
@@ -124,91 +124,114 @@ export function createPropertyPersister(host: powerbi.IVisualHostServices, delay
 }
 
 /**
- * A collection of utils
+ * Returns if there is any more or less data in the new data
+ * @param idEquality Returns true if a and b are referring to the same object, not necessarily if it has changed
  */
-export default class Utils {
-
-    /**
-     * Returns if there is any more or less data in the new data
-     * @param idEquality Returns true if a and b are referring to the same object, not necessarily if it has changed
-     */
-    public static hasDataChanged<T>(newData: T[], oldData: T[], equality: (a: T, b: T) => boolean) {
-        // If the are identical, either same array or undefined, nothing has changed
-        if (oldData === newData) {
-            return false;
-        }
-
-        // If only one of them is undefined or if they differ in length, then its changed
-        if (!oldData || !newData || oldData.length !== newData.length) {
-            return true;
-        }
-
-        // If there are any elements in newdata that arent in the old data
-        return _.some(newData, (n: T) => !_.some(oldData, (m: T) => equality(m, n)));
+export function hasDataChanged<T>(newData: T[], oldData: T[], equality: (a: T, b: T) => boolean) {
+    "use strict";
+    // If the are identical, either same array or undefined, nothing has changed
+    if (oldData === newData) {
+        return false;
     }
 
+    // If only one of them is undefined or if they differ in length, then its changed
+    if (!oldData || !newData || oldData.length !== newData.length) {
+        return true;
+    }
 
-    /**
-     * Diffs the two given lists
-     * @param existingItems The current list of items
-     * @param newItems The new set of items
-     * @param differ The interface for comparing items and add/remove events
-     * @param <M>
-     */
-    // TODO: Think about a param that indicates if should be merged into
-    /// existingItems should be modified, or if only the differ should be called
-    public static listDiff<M>(existingItems: M[], newItems: M[], differ: IDiffProcessor<M>) {
-        existingItems = existingItems || [];
-        newItems = newItems || [];
+    // If there are any elements in newdata that arent in the old data
+    return _.some(newData, (n: T) => !_.some(oldData, (m: T) => equality(m, n)));
+}
 
-        let existing: M;
-        let found: boolean = false;
-        let curr: M;
-        let foundItem: M;
 
-        // Go backwards so we can remove without screwing up the index
-        for (let i = existingItems.length - 1; i >= 0; i--) {
-            existing = existingItems[i];
-            found = false;
-            for (let j = 0; j < newItems.length; j++) {
-                curr = newItems[j];
-                if (differ.equals(curr, existing)) {
-                    found = true;
-                }
-            }
-            if (!found) {
-                existingItems.splice(i, 1);
+function hasDataChanged2(oldOptions: VisualUpdateOptions, newOptions: VisualUpdateOptions) {
+    "use strict";
+    const oldDvs = (oldOptions && oldOptions.dataViews) || [];
+    const dvs = newOptions.dataViews || [];
+    if (oldDvs.length !== dvs.length) {
+        dvs.forEach(dv => markDataViewState(dv));
+        return true;
+    }
+    for (let i = 0; i < oldDvs.length; i++) {
+        if (hasDataViewChanged(oldDvs[i], dvs[i])) {
+            dvs.forEach(dv => markDataViewState(dv));
+            return true;
+        }
+    }
+    dvs.forEach(dv => markDataViewState(dv));
+    return false;
+}
 
-                if (differ.onRemove) {
-                    differ.onRemove(existing);
-                }
+
+/**
+ * Diffs the two given lists
+ * @param existingItems The current list of items
+ * @param newItems The new set of items
+ * @param differ The interface for comparing items and add/remove events
+ * @param <M>
+ */
+// TODO: Think about a param that indicates if should be merged into
+/// existingItems should be modified, or if only the differ should be called
+export function listDiff<M>(existingItems: M[], newItems: M[], differ: IDiffProcessor<M>) {
+    "use strict";
+    existingItems = existingItems || [];
+    newItems = newItems || [];
+
+    let existing: M;
+    let found: boolean = false;
+    let curr: M;
+    let foundItem: M;
+
+    // Go backwards so we can remove without screwing up the index
+    for (let i = existingItems.length - 1; i >= 0; i--) {
+        existing = existingItems[i];
+        found = false;
+        for (let j = 0; j < newItems.length; j++) {
+            curr = newItems[j];
+            if (differ.equals(curr, existing)) {
+                found = true;
             }
         }
+        if (!found) {
+            existingItems.splice(i, 1);
 
-        existing = undefined;
-
-        // Go through the existing ones and add the missing ones
-        for (let i = 0; i < newItems.length; i++) {
-            curr = newItems[i];
-            foundItem = undefined;
-
-            for (let j = 0; j < existingItems.length; j++) {
-                existing = existingItems[j];
-                if (differ.equals(curr, existing)) {
-                    foundItem = existing;
-                }
+            if (differ.onRemove) {
+                differ.onRemove(existing);
             }
-            if (!foundItem) {
-                existingItems.push(curr);
-                if (differ.onAdd) {
-                    differ.onAdd(curr);
-                }
-            } else if (differ.onUpdate) {
-                differ.onUpdate(foundItem, curr);
+        }
+    }
+
+    existing = undefined;
+
+    // Go through the existing ones and add the missing ones
+    for (let i = 0; i < newItems.length; i++) {
+        curr = newItems[i];
+        foundItem = undefined;
+
+        for (let j = 0; j < existingItems.length; j++) {
+            existing = existingItems[j];
+            if (differ.equals(curr, existing)) {
+                foundItem = existing;
             }
+        }
+        if (!foundItem) {
+            existingItems.push(curr);
+            if (differ.onAdd) {
+                differ.onAdd(curr);
+            }
+        } else if (differ.onUpdate) {
+            differ.onUpdate(foundItem, curr);
         }
     }
 }
+
+/**
+ * TODO: Remove this const
+ */
+ export const Utils = { // tslint:disable-line
+  hasDataChanged,
+  listDiff,
+ };
 
 /**
  * Creates an update watcher for a visual
@@ -244,7 +267,7 @@ export function calcUpdateType(oldOpts: VisualUpdateOptions, newOpts: VisualUpda
         updateType ^= UpdateType.Resize;
     }
 
-    if (hasDataChanged(oldOpts, newOpts)) {
+    if (hasDataChanged2(oldOpts, newOpts)) {
         updateType ^= UpdateType.Data;
     }
 
@@ -391,24 +414,6 @@ function hasDataViewChanged(dv1: powerbi.DataView, dv2: powerbi.DataView) {
             return true;
         }
     }
-    return false;
-}
-
-function hasDataChanged(oldOptions: VisualUpdateOptions, newOptions: VisualUpdateOptions) {
-    "use strict";
-    const oldDvs = (oldOptions && oldOptions.dataViews) || [];
-    const dvs = newOptions.dataViews || [];
-    if (oldDvs.length !== dvs.length) {
-        dvs.forEach(dv => markDataViewState(dv));
-        return true;
-    }
-    for (let i = 0; i < oldDvs.length; i++) {
-        if (hasDataViewChanged(oldDvs[i], dvs[i])) {
-            dvs.forEach(dv => markDataViewState(dv));
-            return true;
-        }
-    }
-    dvs.forEach(dv => markDataViewState(dv));
     return false;
 }
 
