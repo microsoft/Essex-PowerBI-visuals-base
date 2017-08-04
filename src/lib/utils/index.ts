2cfb0888aaffe43f6f1e7a054bea8aae8f8e0279
David Tittsworth <stopyoukid@gmail.com>
2017-08-04 14:40:27 -0700

    Removed unused code

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -31,26 +31,13 @@ if (process.env.DEBUG) {
 /* tslint:enable */
 
 export { default as calcUpdateType } from "./calcUpdateType";
-// export * from "./capabilities";
-// export { default as colorizedLog } from "./colorizedLog";
 export { default as createPropertyPersister } from "./createPropertyPersister";
-// export { default as elementLogWriter } from "./elementLogWriter";
-// export { default as hasDataChanged } from "./hasDataChanged";
-// export { default as listDiff } from "./listDiff";
 export * from "./logger";
 export { default as PropertyPersister } from "./PropertyPersister";
-// export * from "./receiveUpdateType";
 export * from "./receiveDimensions";
 export { default as UpdateType } from "./UpdateType";
-// export { default as updateTypeGetter } from "./updateTypeGetter";
-// export { default as Visual } from "./Visual";
-// export { IDiffProcessor } from "./IDiffProcessor";
 export { default as buildContainsFilter } from "./buildContainsFilter";
-// export { default as parseSelectionIds } from "./parseSelectionIds";
 export { default as createPersistObjectBuilder } from "./persistObjectBuilder";
-// export * from "./getSelectionIdsFromSelectors";
-// export { default as get } from "./typesafeGet";
-// export { default as calculateSegments } from "./calculateSegments";
-// export * from "./serialization";
+export { default as get } from "./typesafeGet";
 export * from "./convertItemsWithSegments";
 export * from "./interfaces";
