2d44e22aa377a8a5c8f2a894b87b84fa598f4a83
Chris Trevino <chtrevin@microsoft.com>
2017-08-24 10:35:08 -0700

    Initial Scripts Commit

---
+++ packages/visual-scripts/README.md
@@ -0,0 +1,6 @@
+
+# @essex/powerbi-visual-scripts
+
+This package contains scripts for building PowerBI visuals using the Essex PowerBI build system.
+This build system is designed to emulate the high-level features of the PowerBI-visuals-tools, including the developer visual and bundling capabilities, but using webpack and more normative Typescript idioms (e.g. excluding namespaces, allowing imports).
+
---
+++ packages/visual-scripts/src/essex-pbi-scripts.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+
+'use strict';
+
+process.title = 'essex-pbi-scripts';
+const metadata = require('../package.json');
+const program = require('commander');
+process.env.INIT_CWD = process.cwd();
+const commands = require('./commands');
+
+
+program
+    .version(metadata.version)
+    .on('--version', () => console.log(metadata.version));
+
+program
+    .command('install-certificate')
+    .description('Install Development Certificate')
+    .action(() => commands.installCertificate());
+
+
+program.command('package')
+    .description('Constructe a .pbiviz custom visual file')
+    .action(() => commands.package());
+
+program
+    .command('report')
+    .description('Build Reports')
+    .action((env, options) => {
+        console.log("Build Reports");
+    });
+
+program
+    .command('compile')
+    .description('Compile project assets')
+    .action((env, options) => {
+        console.log("Compile Assets");
+    });
+
+program
+    .command('verify')
+    .description('Compile project assets')
+    .action((env, options) => {
+        console.log("Compile Assets");
+    });
+
+program.command('test')
+    .description('Performs compilation, verification, packaging, and reporting steps')
+    .action((env, options) => {
+        console.log("Package visual");
+    });
+
+program.command('start')
+    .description('Starts the PowerBI Visual Development Server')
+    .action((env, options) => commands.start());
+
+program.command('*').action(() => program.outputHelp());
+
+program.parse(process.argv);
+if (!process.argv.slice(2).length) {
+    program.outputHelp();
+}
