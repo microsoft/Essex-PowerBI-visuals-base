489c0024ecc20bb28fa717d95675df34a0ecaf5c
David Tittsworth <stopyoukid@gmail.com>
2017-09-06 16:30:03 -0700

    Switched some vars to consts

--- run.js
+++ run.js
@@ -1,8 +1,8 @@
 const path = require("path");
-const runArgs = process.argv.slice(2);
 
-let commandToRun = runArgs[0];
-let commandArgs = runArgs.slice(1).join(" ");
+const runArgs = process.argv.slice(2);
+const commandToRun = runArgs[0];
+const commandArgs = runArgs.slice(1).join(" ");
 const resolvedPath = path.resolve(path.join(__dirname, "node_modules", ".bin", commandToRun));
 const child = require("child_process").exec(resolvedPath + (commandArgs.length ? " " + commandArgs : ""));
 child.stderr.pipe(process.stderr);
