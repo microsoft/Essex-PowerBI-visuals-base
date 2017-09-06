5e5b3ed2af90b850de86909222f9095fc08742ab
David Tittsworth <stopyoukid@gmail.com>
2017-09-06 16:16:15 -0700

    More path changes

--- run.js
+++ run.js
@@ -3,7 +3,7 @@ const runArgs = process.argv.slice(2);
 
 let commandToRun = runArgs[0];
 let commandArgs = runArgs.slice(1).join(" ");
-const resolvedPath = path.resolve(path.join(__dirname, ".\\node_modules\\.bin\\", commandToRun));
+const resolvedPath = path.resolve(path.join(__dirname, "node_modules", ".bin", commandToRun));
 const child = require("child_process").exec(resolvedPath + (commandArgs.length ? " " + commandArgs : ""));
 child.stderr.pipe(process.stderr);
 child.stdout.pipe(process.stdout);
